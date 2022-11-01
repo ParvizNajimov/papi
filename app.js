cdf9a50c300e0dac74a5806d7ee3dc65c9db2911
ParvizNajimov <101324794+ParvizNajimov@users.noreply.github.com>
2022-11-02 00:27:10 +0500

    hfh
    gf

---
+++ app.js
@@ -0,0 +1,51 @@
+
+let imya = prompt('what is your name nefr') .toLowerCase() 
+
+if (imya === 'alex') {
+    let account = prompt('account number ??')
+    if(account === '7777') {
+      let money =  prompt(' чан пуль гирам ')
+           if(money <=10000)  {
+           alert("alright")
+          } else {
+         alert('много чего хочешь пацан')
+     }
+    } else {
+    alert('domoy')
+    }
+
+} else {
+  alert('Uyga bor')
+}
+
+
+
+// let name = prompt('what is u name ??') 
+
+
+// if (name[0] === 'a') {
+// let age = prompt('how old are u?') 
+//     if(age <=20 && age >=40 ) {
+//      let cash = prompt("how much money do u have")  
+//         if(cash >=100) {
+//         alert('welcome bro')
+//         } else {
+//             alert('bomj')
+//         }
+
+
+
+
+//      } else {
+//      alert('domoy ')
+//      }
+
+
+
+// } else {
+// alert('domoy soska!!!!')
+// }
+
+
+
+
